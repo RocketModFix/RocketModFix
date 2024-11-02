6db97c6e3bb8755e4b777103b03a2ac5b2edb013
Jonnygaming Tv <22498524+JonnygamingTv@users.noreply.github.com>
2024-11-02 11:12:52 +0100

    Improve dictionary performance

--- Rocket/Rocket.API/Serialisation/RocketPermissions.cs
+++ Rocket/Rocket.API/Serialisation/RocketPermissions.cs
@@ -9,7 +9,7 @@ namespace Rocket.API.Serialisation
     {
         public RocketPermissions()
         {
-            foreach (RocketPermissionsGroup _Group in Groups) GroupsDict[_Group.Id.ToLower()] = _Group;
+            foreach (RocketPermissionsGroup _Group in Groups) GroupsDict[_Group.Id] = _Group;
         }
 
         [XmlElement("DefaultGroup")]
@@ -19,7 +19,7 @@ namespace Rocket.API.Serialisation
         [XmlArrayItem(ElementName = "Group")]
         public List<RocketPermissionsGroup> Groups = new List<RocketPermissionsGroup>();
         [XmlIgnore]
-        public Dictionary<string,RocketPermissionsGroup> GroupsDict = new Dictionary<string,RocketPermissionsGroup>();
+        public Dictionary<string,RocketPermissionsGroup> GroupsDict = new Dictionary<string,RocketPermissionsGroup>(StringComparer.OrdinalIgnoreCase);
         public void LoadDefaults()
         {
             DefaultGroup = "default";
@@ -27,7 +27,7 @@ namespace Rocket.API.Serialisation
                 new RocketPermissionsGroup("default","Guest",null, new List<string>() , new List<Permission>() { new Permission("p"),  new Permission("rocket")},"white"),
                 new RocketPermissionsGroup("vip","VIP", "default",new List<string>() { "76561198016438091" }, new List<Permission>() {  new Permission("effect"), new Permission("heal",120), new Permission("v",30) },"FF9900")
             };
-            foreach (RocketPermissionsGroup _Group in Groups) GroupsDict[_Group.Id.ToLower()] = _Group;
+            foreach (RocketPermissionsGroup _Group in Groups) GroupsDict[_Group.Id] = _Group;
         }
     }
 }
